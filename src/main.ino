2cfbb08b7249088c1060a22f0cdca45377ac35e6
Gedalya Gordon <ghmrgordon@gmail.com>
2025-11-18 16:42:22 -0500

    should be set up for test 1 (precise movement to mission site)

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,8 @@
 
-// #include <Arduino.h>
-// #include <Servo.h>
-// #include "Enes100.h"
-// #include <HX711.h>
+#include <Arduino.h>
+#include <Servo.h>
+#include "Enes100.h"
+#include <HX711.h>
 #include <propulsion.h>
 #include <cstdlib>
 
@@ -48,15 +48,6 @@ void loop() {
       break;
     case ADJUST_POSITION:
       adjust_position();
-      break;
-    case GRAB_CUBE:
-      complete_mission();
-      break;
-    case DROP_CUBE:
-      drop_cube();
-      break;
-    case NAVIGATE_ENDZONE:
-      navigate_to_endzone();
       break;
     default:
       Enes100.println("PANIC! Undefined mission state!");
@@ -123,37 +114,6 @@ void adjust_position() {
   }
 }
 
-void navigate_to_endzone() { // OLD
-  bool checked_left = false;
-  bool in_endzone = false;
-  // Loop through these instructions until we get there
-  while (!in_endzone) {
-    if (close_enough(heading, 0, 5)) { // If we're pointing the right way...
-      // Either avoid obstacles or move forward
-      if (sensor_FL() <= in_front_tolerance || sensor_FR() <= in_front_tolerance) {
-        // Move left. If we get to the wall without clearing in front, move right
-        if (!checked_left) {
-          if (sensor_L() <= in_front_tolerance) checked_left = true;
-           if (!checked_left) move_left(in_front_tolerance);
-        } else {
-          if (sensor_R() <= in_front_tolerance) {
-            // If we've reached the right wall, after checking the left wall,
-            // there is no way forward! Get angry.
-            Enes100.println("PANIC! No clear path found!");
-            break;
-          } else move_right(in_front_tolerance);
-        }
-      } else {
-        move_forward(in_front_tolerance);
-        checked_left = false;
-      }
-    } else { // Otherwise, fix our heading and then assume we're oriented the right way
-      spin(heading);
-      heading = 0;
-    }
-  }
-}
-
 bool close_enough(float f1, float f2) {
   return (abs(f1 - f2) < 0.1);
 }
@@ -172,4 +132,149 @@ float sensor_FR () {
 }
 float sensor_R () {
   return pulseIn(uss_pins[3], HIGH) * 0.034 / 2;
+}
+
+
+
+
+inline constexpr char fr_foward_pin = 0;     // PWM digital
+inline constexpr char fr_backward_pin = 1;   // PWM digital
+
+inline constexpr char fl_foward_pin = 2;     // PWM digital
+inline constexpr char fl_backward_pin = 3;   // PWM digital
+
+inline constexpr char br_foward_pin = 4;     // PWM digital
+inline constexpr char br_backward_pin = 5;   // PWM digital
+
+inline constexpr char bl_foward_pin = 6;     // PWM digital
+inline constexpr char bl_backward_pin = 7;   // PWM digital 
+
+inline constexpr char claw_foward_pin = 8;   // PWM digital
+inline constexpr char claw_backward_pin = 9; // PWM digital
+
+inline constexpr char servo_sg_pin = 10;     // PWM digital
+inline constexpr char servo_feedback = 11;   // analog
+
+inline constexpr char tx_pin = 12; // Wi-Fi Module
+inline constexpr char rx_pin = 13; // Wi-Fi Module
+inline constexpr char aruco_ID = 97;
+
+inline constexpr char loadcell_dout_pin = 14;
+inline constexpr char loadcell_sck_pin = 15;
+
+inline constexpr char uss_pins[4] = {16, 17, 18, 19}; // Ultrasonic Sensors
+
+/* Other constants (FILLER VALUES RIGHT NOW) */
+enum MissionState{
+  GO_TO_CUBE,
+  ADJUST_POSITION,
+  GRAB_CUBE,
+  DROP_CUBE,
+  NAVIGATE_ENDZONE
+};
+
+inline constexpr double fr_nav_input = .15;
+inline constexpr double fl_nav_input = .15;
+inline constexpr double br_nav_input = .15;
+inline constexpr double bl_nav_input = .15;
+
+inline constexpr double nav_speed = 8; // Get through testing Units: cm/sec
+inline constexpr double strafe_speed = 8; // Get through testing Units: cm.sec
+
+inline constexpr double fr_rot_input = .15;
+inline constexpr double fl_rot_input = .15;
+inline constexpr double br_rot_input = .15;
+inline constexpr double bl_rot_input = .15;
+
+inline constexpr double grab_distance = 5.0; // cm, ultrasonic sensor distance to cube to grab it with the claw
+inline constexpr double in_front_tolerance = 10.0; // cm, closest we are willing to get to an obstacle before avoiding it
+inline constexpr double heading_epsilon = 5.0; // degrees, acceptable error in heading when navigating
+inline constexpr double centering_epsilon = 1.0; // cm, acceptable error between front sensors when centering at cube
+inline constexpr double grab_lineal_epsilon = 2.0; // cm, acceptable error in distance to cube when grabbing
+
+inline constexpr double rot_speed = 90.; // degrees, Get through testing Units:rot/sec
+
+//distance in cm
+void move_foward(double distance) {
+  analogWrite(fl_foward_pin, fl_nav_input);
+  analogWrite(fr_foward_pin, fr_nav_input);
+  analogWrite(bl_foward_pin, bl_nav_input);
+  analogWrite(br_foward_pin, br_nav_input);
+  delay((distance/nav_speed)*1000);
+  analogWrite(fl_foward_pin, 0);
+  analogWrite(fr_foward_pin, 0);
+  analogWrite(bl_foward_pin, 0);
+  analogWrite(br_foward_pin, 0);
+}
+
+//distance in cm
+void move_backward(double distance) {
+  analogWrite(fl_backward_pin, fl_nav_input);
+  analogWrite(fr_backward_pin, fr_nav_input);
+  analogWrite(bl_backward_pin, bl_nav_input);
+  analogWrite(br_backward_pin, br_nav_input);
+  delay((distance/nav_speed)*1000);
+  analogWrite(fl_backward_pin, 0);
+  analogWrite(fr_backward_pin, 0);
+  analogWrite(bl_backward_pin, 0);
+  analogWrite(br_backward_pin, 0);
+}
+
+//distance in cm
+void move_right(double distance) {
+  analogWrite(fl_foward_pin, fl_nav_input);
+  analogWrite(fr_backward_pin, fr_nav_input);
+  analogWrite(bl_backward_pin, bl_nav_input);
+  analogWrite(br_foward_pin, br_nav_input);
+  delay((distance/strafe_speed)*1000);
+  analogWrite(fl_foward_pin, 0);
+  analogWrite(fr_backward_pin, 0);
+  analogWrite(bl_backward_pin, 0);
+  analogWrite(br_foward_pin, 0);
+}
+
+//distance in cm
+void move_left(double distance) {
+  analogWrite(fl_backward_pin, fl_nav_input);
+  analogWrite(fr_foward_pin, fr_nav_input);
+  analogWrite(bl_foward_pin, bl_nav_input);
+  analogWrite(br_backward_pin, br_nav_input);
+  delay((distance/strafe_speed)*1000);
+  analogWrite(fl_backward_pin, 0);
+  analogWrite(fr_foward_pin, 0);
+  analogWrite(bl_foward_pin, 0);
+  analogWrite(br_backward_pin, 0);
+}
+
+// degrees > 0 means CW, < 0 means CCW
+void spin(double degs) {
+  if (degs >= 0) {
+    analogWrite(fl_foward_pin, fl_nav_input);
+    analogWrite(fr_backward_pin, fr_nav_input);
+    analogWrite(bl_foward_pin, bl_nav_input);
+    analogWrite(br_backward_pin, br_nav_input);
+    delay((degs/rot_speed)*1000);
+    analogWrite(fl_foward_pin, 0);
+    analogWrite(fr_backward_pin, 0);
+    analogWrite(bl_foward_pin, 0);
+    analogWrite(br_backward_pin, 0);
+  } else {
+    analogWrite(fl_backward_pin, fl_nav_input);
+    analogWrite(fr_foward_pin, fr_nav_input);
+    analogWrite(bl_backward_pin, bl_nav_input);
+    analogWrite(br_foward_pin, br_nav_input);
+    delay((degs/rot_speed)*1000);
+    analogWrite(fl_backward_pin, 0);
+    analogWrite(fr_foward_pin, 0);
+    analogWrite(bl_backward_pin, 0);
+    analogWrite(br_foward_pin, 0);
+  }
+}
+
+void set_servo(Servo servo, double angle) {
+  servo.write(angle);
+}
+
+double read_servo(double angle){
+  return analogRead(servo_feedback);
 }
