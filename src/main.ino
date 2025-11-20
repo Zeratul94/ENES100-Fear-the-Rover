ff9306dfd2acf89b35aad522310317ae32d373ce
andersonderosa <anderson.derosa@gmail.com>
2025-11-20 16:54:35 -0500

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -2,12 +2,64 @@
 #include <Servo.h>
 #include "Enes100.h"
 #include <HX711.h>
-#include <propulsion.h>
-#include <cstdlib>
+//#include <cstdlib>
+//#include <string>
 
 /* Objects */
 Servo claw_servo;
 HX711 scale;
+
+enum MissionState{
+  GO_TO_CUBE,
+  ADJUST_POSITION,
+  GRAB_CUBE,
+  DROP_CUBE,
+  NAVIGATE_ENDZONE
+};
+
+/* SYSTEM PARAMETERS */
+
+// Pins
+char fr_forward_pin = 0;     // PWM digital
+char fr_backward_pin = 1;   // PWM digital
+char fl_forward_pin = 2;     // PWM digital
+char fl_backward_pin = 3;   // PWM digital
+char br_forward_pin = 4;     // PWM digital
+char br_backward_pin = 5;   // PWM digital
+char bl_forward_pin = 6;     // PWM digital
+char bl_backward_pin = 7;   // PWM digital 
+char claw_forward_pin = 8;   // PWM digital
+char claw_backward_pin = 9; // PWM digital
+char servo_sg_pin = 10;     // PWM digital
+char servo_feedback = 11;   // analog
+char tx_pin = 12; // Wi-Fi Module
+char rx_pin = 13; // Wi-Fi Module
+char aruco_ID = 97;
+char loadcell_dout_pin = 14;
+char loadcell_sck_pin = 15;
+char uss_echo_pins[4] = {30, 32, 34, 36}; // Ultrasonic Sensor receive
+char uss_trig_pins[4] = {31, 33, 35, 37}; // Ultrasonic Sensor pulse (should always be HIGH)
+
+// Movement parameters
+double fr_nav_input = .15;
+double fl_nav_input = .15;
+double br_nav_input = .15;
+double bl_nav_input = .15;
+double nav_speed = 8; // Get through testing Units: cm/sec
+double strafe_speed = 8; // Get through testing Units: cm.sec
+double fr_rot_input = .15;
+double fl_rot_input = .15;
+double br_rot_input = .15;
+double bl_rot_input = .15;
+
+
+// Navigation parameters
+double grab_distance = 5.0; // cm, ultrasonic sensor distance to cube to grab it with the claw
+double in_front_tolerance = 10.0; // cm, closest we are willing to get to an obstacle before avoiding it
+double heading_epsilon = 5.0; // degrees, acceptable error in heading when navigating
+double centering_epsilon = 1.0; // cm, acceptable error between front sensors when centering at cube
+double grab_lineal_epsilon = 2.0; // cm, acceptable error in distance to cube when grabbing
+double rot_speed = 90.; // degrees, Get through testing Units:rot/sec
 
 // State values
 MissionState mission_state = GO_TO_CUBE;
@@ -21,6 +73,7 @@ double startpos[2];
 
 void setup() {
   // Initializes ENES100 transmit/receive
+  Serial.begin(9600);
   Enes100.isConnected();
   Enes100.begin("Fear The Rover", MATERIAL, aruco_ID, 1116, tx_pin, rx_pin);
   Enes100.println("Connected...");
@@ -30,6 +83,9 @@ void setup() {
   // Initialize objects
   claw_servo.attach(servo_sg_pin);
   scale.begin(loadcell_dout_pin, loadcell_sck_pin);
+  for (int i=0; i<4; i++) {
+    digitalWrite(uss_trig_pins[i], HIGH);
+  }
 
   // Initialize state
   mission_state = GO_TO_CUBE;
@@ -39,21 +95,30 @@ void setup() {
 }
 
 void loop() {
-  heading = Enes100.getTheta();
-  position[0] = Enes100.getX(); position[1] = Enes100.getY();
-  switch (mission_state) {
-    case GO_TO_CUBE:
-      navigate_to_mission();
-      break;
-    case ADJUST_POSITION:
-      adjust_position();
-      break;
-    default:
-      Enes100.println("PANIC! Undefined mission state!");
-      break;
-  }
+  Serial.println("Starting frame");
+  // heading = Enes100.getTheta();
+  // position[0] = Enes100.getX(); position[1] = Enes100.getY();
+  // switch (mission_state) {
+  //   case GO_TO_CUBE:
+  //     navigate_to_mission();
+  //     break;
+  //   case ADJUST_POSITION:
+  //     adjust_position();
+  //     break;
+  //   default:
+  //     Enes100.println("PANIC! Undefined mission state!");
+  //     break;
+  // }
+  Serial.println("L: ");
+  Serial.print(sensor_L());
+  Serial.println("FL: ");
+  Serial.print(sensor_FL());
+  Serial.println("FR: ");
+  Serial.print(sensor_FR());
+  Serial.println("R: ");
+  Serial.print(sensor_R());
 
-  loopctr++;
+  //loopctr++;
 }
 
 void navigate_to_mission() {
@@ -133,82 +198,17 @@ float sensor_R () {
   return pulseIn(uss_echo_pins[3], HIGH) * 0.034 / 2;
 }
 
-
-/* SYSTEM PARAMETERS */
-
-// Pins
-inline constexpr char fr_foward_pin = 0;     // PWM digital
-inline constexpr char fr_backward_pin = 1;   // PWM digital
-
-inline constexpr char fl_foward_pin = 2;     // PWM digital
-inline constexpr char fl_backward_pin = 3;   // PWM digital
-
-inline constexpr char br_foward_pin = 4;     // PWM digital
-inline constexpr char br_backward_pin = 5;   // PWM digital
-
-inline constexpr char bl_foward_pin = 6;     // PWM digital
-inline constexpr char bl_backward_pin = 7;   // PWM digital 
-
-inline constexpr char claw_foward_pin = 8;   // PWM digital
-inline constexpr char claw_backward_pin = 9; // PWM digital
-
-inline constexpr char servo_sg_pin = 10;     // PWM digital
-inline constexpr char servo_feedback = 11;   // analog
-
-inline constexpr char tx_pin = 12; // Wi-Fi Module
-inline constexpr char rx_pin = 13; // Wi-Fi Module
-inline constexpr char aruco_ID = 97;
-
-inline constexpr char loadcell_dout_pin = 14;
-inline constexpr char loadcell_sck_pin = 15;
-
-inline constexpr char uss_echo_pins[4] = {30, 32, 34, 36}; // Ultrasonic Sensor receive
-inline constexpr char uss_trig_pins[4] = {31, 33, 35, 37}; // Ultrasonic Sensor pulse (should always be HIGH)
-
-// Movement parameters
-inline constexpr double fr_nav_input = .15;
-inline constexpr double fl_nav_input = .15;
-inline constexpr double br_nav_input = .15;
-inline constexpr double bl_nav_input = .15;
-
-inline constexpr double nav_speed = 8; // Get through testing Units: cm/sec
-inline constexpr double strafe_speed = 8; // Get through testing Units: cm.sec
-
-inline constexpr double fr_rot_input = .15;
-inline constexpr double fl_rot_input = .15;
-inline constexpr double br_rot_input = .15;
-inline constexpr double bl_rot_input = .15;
-
-
-// Navigation parameters
-inline constexpr double grab_distance = 5.0; // cm, ultrasonic sensor distance to cube to grab it with the claw
-inline constexpr double in_front_tolerance = 10.0; // cm, closest we are willing to get to an obstacle before avoiding it
-inline constexpr double heading_epsilon = 5.0; // degrees, acceptable error in heading when navigating
-inline constexpr double centering_epsilon = 1.0; // cm, acceptable error between front sensors when centering at cube
-inline constexpr double grab_lineal_epsilon = 2.0; // cm, acceptable error in distance to cube when grabbing
-
-inline constexpr double rot_speed = 90.; // degrees, Get through testing Units:rot/sec
-
-
-enum MissionState{
-  GO_TO_CUBE,
-  ADJUST_POSITION,
-  GRAB_CUBE,
-  DROP_CUBE,
-  NAVIGATE_ENDZONE
-};
-
 //distance in cm
-void move_foward(double distance) {
-  analogWrite(fl_foward_pin, fl_nav_input);
-  analogWrite(fr_foward_pin, fr_nav_input);
-  analogWrite(bl_foward_pin, bl_nav_input);
-  analogWrite(br_foward_pin, br_nav_input);
+void move_forward(double distance) {
+  analogWrite(fl_forward_pin, fl_nav_input);
+  analogWrite(fr_forward_pin, fr_nav_input);
+  analogWrite(bl_forward_pin, bl_nav_input);
+  analogWrite(br_forward_pin, br_nav_input);
   delay((distance/nav_speed)*1000);
-  analogWrite(fl_foward_pin, 0);
-  analogWrite(fr_foward_pin, 0);
-  analogWrite(bl_foward_pin, 0);
-  analogWrite(br_foward_pin, 0);
+  analogWrite(fl_forward_pin, 0);
+  analogWrite(fr_forward_pin, 0);
+  analogWrite(bl_forward_pin, 0);
+  analogWrite(br_forward_pin, 0);
 }
 
 //distance in cm
@@ -226,52 +226,52 @@ void move_backward(double distance) {
 
 //distance in cm
 void move_right(double distance) {
-  analogWrite(fl_foward_pin, fl_nav_input);
+  analogWrite(fl_forward_pin, fl_nav_input);
   analogWrite(fr_backward_pin, fr_nav_input);
   analogWrite(bl_backward_pin, bl_nav_input);
-  analogWrite(br_foward_pin, br_nav_input);
+  analogWrite(br_forward_pin, br_nav_input);
   delay((distance/strafe_speed)*1000);
-  analogWrite(fl_foward_pin, 0);
+  analogWrite(fl_forward_pin, 0);
   analogWrite(fr_backward_pin, 0);
   analogWrite(bl_backward_pin, 0);
-  analogWrite(br_foward_pin, 0);
+  analogWrite(br_forward_pin, 0);
 }
 
 //distance in cm
 void move_left(double distance) {
   analogWrite(fl_backward_pin, fl_nav_input);
-  analogWrite(fr_foward_pin, fr_nav_input);
-  analogWrite(bl_foward_pin, bl_nav_input);
+  analogWrite(fr_forward_pin, fr_nav_input);
+  analogWrite(bl_forward_pin, bl_nav_input);
   analogWrite(br_backward_pin, br_nav_input);
   delay((distance/strafe_speed)*1000);
   analogWrite(fl_backward_pin, 0);
-  analogWrite(fr_foward_pin, 0);
-  analogWrite(bl_foward_pin, 0);
+  analogWrite(fr_forward_pin, 0);
+  analogWrite(bl_forward_pin, 0);
   analogWrite(br_backward_pin, 0);
 }
 
 // degrees > 0 means CW, < 0 means CCW
 void spin(double degs) {
   if (degs >= 0) {
-    analogWrite(fl_foward_pin, fl_nav_input);
+    analogWrite(fl_forward_pin, fl_nav_input);
     analogWrite(fr_backward_pin, fr_nav_input);
-    analogWrite(bl_foward_pin, bl_nav_input);
+    analogWrite(bl_forward_pin, bl_nav_input);
     analogWrite(br_backward_pin, br_nav_input);
     delay((degs/rot_speed)*1000);
-    analogWrite(fl_foward_pin, 0);
+    analogWrite(fl_forward_pin, 0);
     analogWrite(fr_backward_pin, 0);
-    analogWrite(bl_foward_pin, 0);
+    analogWrite(bl_forward_pin, 0);
     analogWrite(br_backward_pin, 0);
   } else {
     analogWrite(fl_backward_pin, fl_nav_input);
-    analogWrite(fr_foward_pin, fr_nav_input);
+    analogWrite(fr_forward_pin, fr_nav_input);
     analogWrite(bl_backward_pin, bl_nav_input);
-    analogWrite(br_foward_pin, br_nav_input);
+    analogWrite(br_forward_pin, br_nav_input);
     delay((degs/rot_speed)*1000);
     analogWrite(fl_backward_pin, 0);
-    analogWrite(fr_foward_pin, 0);
+    analogWrite(fr_forward_pin, 0);
     analogWrite(bl_backward_pin, 0);
-    analogWrite(br_foward_pin, 0);
+    analogWrite(br_forward_pin, 0);
   }
 }
 
